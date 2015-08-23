2bbfbaa7d86ed42ad7239e18621f15c6d77642f6
william-bratches <william.bratches@gmail.com>
2015-08-23 17:21:03 -0400

    added index file grabber

---
+++ idxGrab.py
@@ -0,0 +1,18 @@
+import wget
+import os
+
+
+root_url = "ftp://ftp.sec.gov/edgar/full-index/"#2005/QTR3/form.idx"
+os.chdir('data/idx') #download everything here
+
+def grabIdx(root_url):
+    for year in range (1993, 2015):
+        for quarter in range (1, 4):
+            local_url = root_url + str(year) + "/" + "QTR" + str(quarter) + "/form.idx"
+
+            # save form in data
+            wget.download(local_url)
+
+if __name__ == '__main__':
+    grabIdx(root_url)
+
--- scrape.js
+++ scrape.js
@@ -4,41 +4,46 @@ var request = require('request');
 var cheerio = require('cheerio');
 var app     = express();
 
-app.get('/scrape', function(req, res){
-    // Let's scrape Anchorman 2
-    url = 'http://www.imdb.com/title/tt1229340/';
+// all years in SEC EDGAR, 1993-2015
+for (var i = 2015 - 1; i >= 1993; i--) {
+    
+    app.get('/scrape', function(req, res){
+        // Let's scrape Anchorman 2
+        rootUrl = 'ftp://ftp.sec.gov/edgar/full-index/2005/QTR3/form.idx';
 
-    request(url, function(error, response, html){
-        if(!error){
-            var $ = cheerio.load(html);
+        request(url, function(error, response, html){
+            if(!error){
+                var $ = cheerio.load(html);
 
-            var title, release, rating;
-            var json = { title : "", release : "", rating : ""};
+                var title, release, rating;
+                var json = { title : "", release : "", rating : ""};
 
-            $('.header').filter(function(){
-                var data = $(this);
-                title = data.children().first().text();
-                release = data.children().last().children().text();
+                $('.header').filter(function(){
+                    var data = $(this);
+                    title = data.children().first().text();
+                    release = data.children().last().children().text();
 
-                json.title = title;
-                json.release = release;
+                    json.title = title;
+                    json.release = release;
+                })
+
+                $('.star-box-giga-star').filter(function(){
+                    var data = $(this);
+                    rating = data.text();
+
+                    json.rating = rating;
+                })
+            }
+
+            fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err){
+                console.log('File successfully written! - Check your project directory for the output.json file');
             })
 
-            $('.star-box-giga-star').filter(function(){
-                var data = $(this);
-                rating = data.text();
-
-                json.rating = rating;
-            })
-        }
-
-        fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err){
-            console.log('File successfully written! - Check your project directory for the output.json file');
+            res.send('Check your console!')
         })
-
-        res.send('Check your console!')
     })
-})
+
+};
 
 app.listen('8081')
 console.log('Magic happens on port 8081');
