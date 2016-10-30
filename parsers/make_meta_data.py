218532f792c5388dc6094d835b6dfebf9c65977a
william-bratches <william.bratches@gmail.com>
2016-10-30 19:41:49 -0400

    wip

---
+++ parsers/make_meta_data.py
@@ -0,0 +1,18 @@
+# in javascript
+# Create metadata for the bookworm.
+
+const fs = require('fs');
+
+const readFile = () => {};
+
+const determineIndustry = () => {};
+
+const parse = (fileString) => {};
+
+# {
+   "searchstring" : companyName,
+                 "filename" : filename,
+                 "year" : year,
+                 "industry" : subjectArray,
+		 "ticker": ticker, ??
+                }
