69716c1cd2758762312412f7e7763627a0b65bca
william-bratches <william.bratches@gmail.com>
2016-11-01 15:55:23 -0400

    updates

--- parsers/make_meta_data.js
+++ parsers/make_meta_data.js
@@ -1,23 +1,66 @@
 const fs = require('fs');
-const industries = require('../data/industries');
-
 const dataPath = '../data/10K';
 
-const determineIndustry = () => {};
+const output = '../data/bookworm/jsoncatalog.txt';
 
-const parse = (fileString) => {};
+const stripIndexKey = (indexKey) => {
+  return for (let i = 0; i < indexKey.length; i++) {
+    if (indexKey[i] !== 0) {
+      return indexKey.slice(i);
+    }
+  }
+}
 
-const parseAll = (directory) => {
+const getIndustry = (lines) => {
+  return lines[24].split(':')[1].trim();
+};
+
+const getCompanyName = (lines) => {
+  return lines[22].split(':')[1].trim();
+};
+
+const getReportYear = (lines) => {
+  return lines[13].split('-')[1];
+};
+
+const createSearchString = (fileName, lines) => {
+  const root = 'https://www.sec.gov/Archives/edgar/data'
+  const fileNameStripped = fileName.replace('-', '').replace('.txt', '');
+  const indexKey = lines[23].replace(' ', '').split(':')[1];
+  const indexKeyStripped = stripIndexKey(indexKey);
+  return `${root}$/${indexKeyStripped}/${fileNameStripped}/${fileName}`;
+};
+
+const readAndParse = (file, cb) => {
+  fs.readFile(`${directory}/${file}`, 'utf8', (err, data) => {
+    if (err) {
+      cb(err);
+    }
+    const lines = data.toString().split('\n');
+
+    return {
+      searchString: createSearchString(file, lines),
+      companyName: getCompanyName(lines),
+      year: getReportYear(lines),
+      industry: getCompanyIndustry(lines),
+      filename: file,
+    };
+  });
+};
+
+const writeMeta = (line, output) => {};
+
+const parseAndWriteAll = (directory) => {
   fs.readdir(directory, (err, files) => {
     if (err) {
       return console.log('ERROR:', err);
     }
-    return files.forEach((file) => {
-      fs.readFile(`${directory}/${file}`, 'utf8', (err, data) => {
-        if (err) {
-          return console.log(`Err reading file ${file}`);
+    files.forEach((file) => {
+      readAndParse(file, (parseErr, data) => {
+        if (parseErr) {
+          return console.log(`Err reading file ${file}`)
         }
-        console.log(data);
+        writeMeta(metaData, output);
       });
     });
   });
@@ -26,9 +69,9 @@ const parseAll = (directory) => {
 parseAll(dataPath);
 
  // {
- //   "searchstring" : companyName,
- //   "filename" : filename,
+ //   "searchstring" : 10K_link, https://www.sec.gov/Archives/edgar/data/1490196/000149019610000002/0001490196-10-000002.txt
+ //   "companyName": companyName, // can categorize
  //   "year" : year,
- //   "industry" : subjectArray,
- // 	 "ticker": ticker, ??
+ //   "filename" : filename,
+ //   "industry" : subjectArray, // can categorize
  // }
