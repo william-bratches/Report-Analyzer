6ad9b996c1fa8852b27ea4f4a0706a229d901d99
william-bratches <william.bratches@gmail.com>
2016-11-15 18:54:14 -0500

    fixed parsing

--- parsers/make_meta_data.js
+++ parsers/make_meta_data.js
@@ -1,54 +1,61 @@
 const fs = require('fs');
 const dataPath = '../data/10K';
-
+const _ = require('lodash');
 const output = '../data/bookworm/jsoncatalog.txt';
 
+const findLine = (regex, lines) => {
+  return _.find(lines, (line) => {
+    return line.match(regex);
+  });
+};
+
 const stripIndexKey = (indexKey) => {
-  return for (let i = 0; i < indexKey.length; i++) {
+  for (var i = 0; i < indexKey.length; i++) {
     if (indexKey[i] !== 0) {
       return indexKey.slice(i);
     }
   }
-}
+};
 
 const getIndustry = (lines) => {
-  return lines[24].split(':')[1].trim();
+  return findLine('STANDARD INDUSTRIAL CLASSIFICATION', lines).split(':')[1].trim();
 };
 
 const getCompanyName = (lines) => {
-  return lines[22].split(':')[1].trim();
+  return findLine('COMPANY CONFORMED NAME', lines).split(':')[1].trim();
 };
 
 const getReportYear = (lines) => {
-  return lines[13].split('-')[1];
+  return findLine('ACCESSION NUMBER', lines).split('-')[1];
 };
 
 const createSearchString = (fileName, lines) => {
   const root = 'https://www.sec.gov/Archives/edgar/data'
   const fileNameStripped = fileName.replace('-', '').replace('.txt', '');
-  const indexKey = lines[23].replace(' ', '').split(':')[1];
+  const indexKey = findLine('CENTRAL INDEX KEY', lines).replace(' ', '').split(':')[1];
   const indexKeyStripped = stripIndexKey(indexKey);
   return `${root}$/${indexKeyStripped}/${fileNameStripped}/${fileName}`;
 };
 
-const readAndParse = (file, cb) => {
+const readAndParse = (file, directory, cb) => {
   fs.readFile(`${directory}/${file}`, 'utf8', (err, data) => {
     if (err) {
       cb(err);
     }
     const lines = data.toString().split('\n');
-
-    return {
+    cb(null, {
       searchString: createSearchString(file, lines),
       companyName: getCompanyName(lines),
       year: getReportYear(lines),
-      industry: getCompanyIndustry(lines),
+      industry: getIndustry(lines),
       filename: file,
-    };
+    });
   });
 };
 
-const writeMeta = (line, output) => {};
+const writeMeta = (metadata, output) => {
+  console.log(metadata);
+};
 
 const parseAndWriteAll = (directory) => {
   fs.readdir(directory, (err, files) => {
@@ -56,17 +63,17 @@ const parseAndWriteAll = (directory) => {
       return console.log('ERROR:', err);
     }
     files.forEach((file) => {
-      readAndParse(file, (parseErr, data) => {
+      readAndParse(file, directory, (parseErr, data) => {
         if (parseErr) {
           return console.log(`Err reading file ${file}`)
         }
-        writeMeta(metaData, output);
+        writeMeta(data, output);
       });
     });
   });
 };
 
-parseAll(dataPath);
+parseAndWriteAll(dataPath);
 
  // {
  //   "searchstring" : 10K_link, https://www.sec.gov/Archives/edgar/data/1490196/000149019610000002/0001490196-10-000002.txt
